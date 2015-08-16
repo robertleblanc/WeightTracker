4a5fefe7eb9206b706efb398445d2816b888e6f6
Robert LeBlanc <robert.leblanc365@gmail.com>
2015-08-15 23:44:17 -0600

    Initial commit

---
+++ app/src/main/java/rleblanc/ca/weighttracker/Weight_Tracker_GUIFragment.java
@@ -0,0 +1,22 @@
+package rleblanc.ca.weighttracker;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Weight_Tracker_GUIFragment extends Fragment {
+
+    public Weight_Tracker_GUIFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weight_graph, container, false);
+    }
+}
